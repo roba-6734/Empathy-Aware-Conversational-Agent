11e27be17fba61a2f5c95da2c59865721c1812f4
roba-6734 <roberaasmera@gmail.com>
2026-01-03 01:31:00 +0400

    add: src/multimodal_empathy/config.py

---
+++ src/multimodal_empathy/config.py
@@ -0,0 +1,7 @@
+"""Central configuration values for the MVP."""
+
+TARGET_EMOTIONS = ["joy", "sadness", "anger", "frustration", "neutral"]
+
+DEFAULT_TEXT_MODEL = "j-hartmann/emotion-english-distilroberta-base"
+DEFAULT_VISION_MODEL = "dima806/facial_emotions_image_detection"
+DEFAULT_RESPONSE_MODEL = "google/flan-t5-small"
